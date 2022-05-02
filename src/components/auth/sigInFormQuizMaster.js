b61135469dfcac9de745a4200ad6d1218e575ab5
marwa <marwaannebi25@gmail.com>
2022-05-02 16:39:58 +0100

    authentificationFirstCommit

--- src/components/auth/sigInFormQuizMaster.js
+++ src/components/auth/sigInFormQuizMaster.js
@@ -4,57 +4,150 @@ import iconPlay from "./../../assets/polygone-2-1@1x.png";
 import Icon from "../icon";
 import { FaGoogle, FaLinkedinIn, FaMicrosoft } from "react-icons/fa";
 import { AccountContext } from "../accountContext";
+import { Button, makeStyles, TextField } from "@material-ui/core";
+import axios from "axios";
+import Loading from "../Loading";
+import Notification from "../Notification";
+const styles = makeStyles({
+  textField: {
+    width: "70%",
+    marginLeft: "-20px",
+    // marginRight: 'auto',
+    paddingBottom: 0,
+    marginTop: 0,
+    fontWeight: 500,
+  },
+
+  input: {
+    color: "black",
+    fontFamily: "cerapro-Medium",
+    // font-size: var(--font-size-m);
+  },
+  label: {
+    fontFamily: "cerapro-bold",
+    color: "#560a02",
+    fontSize: "20px",
+    fontWeight: 700,
+    opacity: 0.48,
+    whiteSpace: "nowrap",
+  },
+});
+
 export default function SigInForm() {
+  const google = () => {
+    window.open("http://localhost:5000/auth/google/Quizmaster", "_self");
+  };
+
+  const linkedin = () => {
+    window.open("http://localhost:5000/auth/linkedin/Quizmaster", "_self");
+  };
+
+  const microsoft = () => {
+    window.open("http://localhost:5000/auth/microsoft/Quizmaster", "_self");
+  };
+
   const { switchToSignup } = useContext(AccountContext);
+  const classes = styles();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setloading] = useState(false);
+  const [notify, setNotify] = useState({
+    isOpen: false,
+    message: "",
+    type: "",
+  });
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      setloading(true);
+      const { data } = await axios.post(
+        "/auth/loginUser",
+        { email, password, type: "3" },
+        config
+      );
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setloading(false);
+    } catch (error) {
+      setloading(false);
+
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setNotify({
+          isOpen: true,
+          message: error.response.data.message,
+          type: "error",
+        });
+      }
+    }
+  };
 
   return (
-
-    <div className="rectangle-white">
-      <h1 className="title">log in as QuizMaster</h1>
-      <div class="form__group field">
-        <input
-          type="email"
-          class="form__field"
-          placeholder="Email"
-          name="email"
-          id="email"
-          required
-        />
-        <label for="email" className="form__label">
-          Email
-        </label>
-      </div>
-
-      <div class="form__group field">
-        <input
-          type="password"
-          class="form__field"
-          placeholder="Password"
-          name="password"
-          id="password"
-          required
-        />
-        <label for="password" class="form__label1">
-          Password
-        </label>
-      </div>
-      <img src={iconPlay} className="iconPlay" />
-      <h4 className="lost-your-password">Lost your password ?</h4>
-      <div className="IconsContainer">
-        <Icon >
-          <FaGoogle size={"40px"} />
-        </Icon>
-        <Icon>
-          <FaLinkedinIn size={"40px"} />
-        </Icon>
-        <Icon>
-          <FaMicrosoft size={"40px"} />
-        </Icon>
-      </div>
-      <div style={{ marginTop: "50px" }}>
-        <a href="#" className="btn-sign-up" onClick={switchToSignup}>
-          sign up
-        </a>
+    <div>
+      {loading && <Loading />}
+      <Notification notify={notify} setNotify={setNotify} />
+      <div className="rectangle-white">
+        <h1 className="title">log in as Quiz master </h1>
+        <form onSubmit={submitHandler} style={{ flexDirection: "column" }}>
+          <TextField
+            type="email"
+            // class="form__field"
+            label="Email"
+            value={email}
+            id="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            className={classes.textField}
+            InputProps={{
+              className: classes.input,
+            }}
+            InputLabelProps={{ className: classes.label }}
+          />
+          <br />
+          <TextField
+            type="password"
+            // class="form__field"
+            label="Password"
+            value={password}
+            id="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+            className={classes.textField}
+            InputProps={{
+              className: classes.input,
+            }}
+            InputLabelProps={{ className: classes.label }}
+          />
+          <br />
+          <Button variant="primary" type="submit">
+            <img src={iconPlay} className="iconPlay" />
+          </Button>
+          <h4 className="lost-your-password">Lost your password ?</h4>
+          <div className="IconsContainer">
+            <Icon onclick={google}>
+              <FaGoogle size={"40px"} />
+            </Icon>
+            <Icon onclick={linkedin}>
+              <FaLinkedinIn size={"40px"} />
+            </Icon>
+            <Icon onclick={microsoft}>
+              <FaMicrosoft size={"40px"} />
+            </Icon>
+          </div>
+          <div style={{ marginTop: "50px" }}>
+            <a href="#" className="btn-sign-up" onClick={switchToSignup}>
+              sign up
+            </a>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/components/auth/signUpCandidate.js
+++ src/components/auth/signUpCandidate.js
@@ -1,97 +1,251 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import iconPlay from "./../../assets/polygone-2-1@1x.png";
-
+import "./../../css/signup.css";
 import Icon from "../icon";
 import { FaGoogle, FaLinkedinIn, FaMicrosoft } from "react-icons/fa";
 import { AccountContext } from "../accountContext";
+import { Button, makeStyles, TextField } from "@material-ui/core";
+import axios from "axios";
+import Loading from "../Loading";
+import PasswordStrengthBar from "react-password-strength-bar";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import Notification from "../Notification";
+
+const styles = makeStyles({
+  textField: {
+    width: "70%",
+    marginLeft: "-20px",
+    // marginRight: 'auto',
+    paddingBottom: 0,
+    marginTop: 0,
+    fontWeight: 500,
+  },
+
+  input: {
+    color: "black",
+    fontFamily: "cerapro-Medium",
+    // font-size: var(--font-size-m);
+  },
+  label: {
+    fontFamily: "cerapro-bold",
+    color: "#560a02",
+    fontSize: "17px",
+    fontWeight: 700,
+    opacity: 0.48,
+    whiteSpace: "nowrap",
+  },
+});
 export default function SignUpCandidate() {
+  const google = () => {
+    window.open("http://localhost:5000/auth/google/Candidate", "_self");
+  };
+
+  const linkedin = () => {
+    window.open("http://localhost:5000/auth/linkedin/Candidate", "_self");
+  };
+
+  const microsoft = () => {
+    window.open("http://localhost:5000/auth/microsoft/Candidate", "_self");
+  };
   const { switchToSignin } = useContext(AccountContext);
+  const classes = styles();
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setlastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [loading, setloading] = useState(false);
+  const [notify, setNotify] = useState({
+    isOpen: false,
+    message: "",
+    type: "",
+  });
+  const [showPassword, setshowPassword] = useState(false);
+  const handleClickShowPassword = () => {
+    setshowPassword(true);
+  };
+  const handleMouseDownPassword = () => {
+    setshowPassword(false);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmpassword) {
+      setNotify({
+        isOpen: true,
+        message: "passwords do not match",
+        type: "error",
+      });
+    } else {
+      try {
+        const config = {
+          headers: {
+            "Content-type": "application/json",
+          },
+        };
+        setloading(true);
+        const { data } = await axios.post(
+          "/auth/registerCandidate",
+          { firstName, lastName, email, password },
+          config
+        );
+        setNotify({
+          isOpen: true,
+          message: "verify your email ,check your inbox",
+          type: "success",
+        });
+        setloading(false);
+        localStorage.setItem("userInfo", JSON.stringify(data));
+      } catch (error) {
+        if (
+          error.response &&
+          error.response.status >= 400 &&
+          error.response.status <= 500
+        ) {
+          setNotify({
+            isOpen: true,
+            message: error.response.data.message,
+            type: "error",
+          });
+          setloading(false);
+        }
+      }
+    }
+  };
 
   return (
-    <div className="rectangle-white">
-      <h1 className="title1">Register as Candidate</h1>
-      <div class="form__group1 field1">
-        <input
-          type="text"
-          class="form__field"
-          placeholder="Password"
-          name="password"
-          id="password"
-          required
-        />
-        <label for="password" class="form__label1">
-          First name
-        </label>
-      </div>
-      <div class="form__group1 field1">
-        <input
-          type="text"
-          class="form__field"
-          placeholder="Password"
-          name="password"
-          id="password"
-          required
-        />
-        <label for="password" class="form__label1">
-          Last name
-        </label>
-      </div>
-      <div class="form__group1 field1">
-        <input
-          type="email"
-          class="form__field"
-          placeholder="Email"
-          name="email"
-          id="email"
-          required
-        />
-        <label for="email" className="form__label">
-          Email
-        </label>
-      </div>
+    <div>
+      {loading && <Loading />}
+      <Notification notify={notify} setNotify={setNotify} />
+      <div className="rectangle-white">
+        <h1 className="title1">Register as Candidate</h1>
+        <form onSubmit={submitHandler} style={{ flexDirection: "column" }}>
+          <TextField
+            type="text"
+            // class="form__field"
+            label="first Name"
+            id="firstName"
+            // required
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className={classes.textField}
+            InputProps={{
+              className: classes.input,
+            }}
+            InputLabelProps={{ className: classes.label }}
+          />
+          <br />
+          <TextField
+            type="text"
+            // class="form__field"
+            label="last Name"
+            value={lastName}
+            id="lastName"
+            // required
+            className={classes.textField}
+            onChange={(e) => setlastName(e.target.value)}
+            InputProps={{
+              className: classes.input,
+            }}
+            InputLabelProps={{ className: classes.label }}
+          />
 
-      <div class="form__group1 field1">
-        <input
-          type="password"
-          class="form__field"
-          placeholder="Password"
-          name="password"
-          id="password"
-          required
-        />
-        <label for="password" class="form__label1">
-          Password
-        </label>
-      </div>
-      <div class="form__group1 field1">
-        <input
-          type="password"
-          class="form__field"
-          placeholder="Password"
-          name="password"
-          id="password"
-          required
-        />
-        <label for="password" class="form__label1">
-          Confirm password
-        </label>
-      </div>
-      <img src={iconPlay} className="iconPlay1" />
-      <div className="IconsContainer1">
-        <Icon>
-          <FaGoogle size={"40px"} />
-        </Icon>
-        <Icon>
-          <FaLinkedinIn size={"40px"} />
-        </Icon>
-        <Icon>
-          <FaMicrosoft size={"40px"} />
-        </Icon>
-      </div>
-      <div style={{ marginTop: "25px" }}>
-        <a href="#" className="btn-sign-up1" onClick={switchToSignin}>
-          sign in
-        </a>
+          <br />
+
+          <TextField
+            type="email"
+            // class="form__field"
+            label="email"
+            value={email}
+            id="email"
+            // onError={error.email}
+            // required
+            onChange={(e) => setEmail(e.target.value)}
+            className={classes.textField}
+            InputProps={{
+              className: classes.input,
+            }}
+            InputLabelProps={{ className: classes.label }}
+          />
+          <br />
+
+          <TextField
+            type={showPassword ? "text" : "password"}
+            // class="form__field"
+            label="password"
+            value={password}
+            id="filled-adornment-password"
+            // required
+            onChange={(e) => setPassword(e.target.value)}
+            className={classes.textField}
+            InputProps={{
+              className: classes.input,
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    style={{ color: "#560a02" }}
+                  >
+                    {showPassword && <Visibility />}
+                    {!showPassword && <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            InputLabelProps={{ className: classes.label }}
+          />
+
+          <PasswordStrengthBar
+            password={password}
+            style={{
+              width: "300px",
+              marginLeft: "60px",
+              marginBottom: "-20px",
+            }}
+          />
+
+          <TextField
+            type={showPassword ? "text" : "password"}
+            // class="form__field"
+            label="confirm password"
+            value={confirmpassword}
+            id="confirmpassword"
+            // required
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className={classes.textField}
+            InputProps={{
+              className: classes.input,
+            }}
+            InputLabelProps={{ className: classes.label }}
+          />
+          <br />
+
+          <Button variant="primary" type="submit">
+            <img src={iconPlay} className="iconPlay1" />
+          </Button>
+        </form>
+        <div className="IconsContainer1">
+          <Icon onclick={google}>
+            <FaGoogle size={"40px"} />
+          </Icon>
+          <Icon onclick={linkedin}>
+            <FaLinkedinIn size={"40px"} />
+          </Icon>
+          <Icon onclick={microsoft}>
+            <FaMicrosoft size={"40px"} />
+          </Icon>
+        </div>
+
+        <div style={{ marginTop: "45px" }}>
+          <a href="#" className="btn-sign-up1" onClick={switchToSignin}>
+            sign in
+          </a>
+        </div>
       </div>
     </div>
   );
