6cdc40ec2f47a096296ece95aeb113854f8e7391
marwa <marwaannebi25@gmail.com>
2022-06-14 23:45:08 +0100

    quiz

--- src/components/candidate/PlayQuiz.js
+++ src/components/candidate/PlayQuiz.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import { Checkbox, TablePagination } from "@material-ui/core";
 import { useLocation } from "react-router-dom";
 import Typography from "@mui/material/Typography";
@@ -36,32 +36,45 @@ export default function PlayQuiz(company_info) {
   const [rowsPerPage, setRowsPerPage] = useState(1);
   const id = state.quiz;
   const [error, seterror] = useState();
+  const [minutes, setminutes] = useState();
+  const [seconds, setseconds] = useState();
+  const [timer, setTimer] = useState("00:00");
+
   const fetchUsers = async () => {
     try {
-      const { result, countQuestion, compteur, message } = await read({
+      const { result, countQuestion, message, minutes, seconds } = await read({
         page: page,
         perPage: rowsPerPage,
         id: id,
       });
       setquiz(result);
       setCount(countQuestion);
-      setcompteur(compteur);
+      // setcompteur(compteur);
+      setminutes(minutes);
+      setseconds(seconds);
+      seterror(message);
+      console.log(minutes);
+      console.log(seconds);
+
       // const msg = message;
     } catch (error) {
       alert(error);
     }
   };
-  const handleCheck = (event, _id) => {
+
+  const handleCheck = (event, _id, question) => {
     var updatedList = [...answercandidate];
     if (event.target.checked) {
       let add = {
         _id_proposition: _id,
+        _id_Question: question,
         response: event.target.checked,
       };
       updatedList = [...answercandidate, add];
     } else {
       updatedList.splice(answercandidate.indexOf(event.target.value), 1);
     }
+    // else if(answercandidate)
     setanswercandidate(updatedList);
   };
   useEffect(() => {
@@ -103,115 +116,148 @@ export default function PlayQuiz(company_info) {
             boxShadow: " 0px 3px 6px #00000029",
           }}
         >
-          <form>
-            <Grid container spacing={2}>
-              <Grid item xs={5}>
-                <div
-                  style={{
-                    border: `4px solid ${darkColor}`,
-                    borderRadius: "39px",
-                    width: "60px",
-                    height: "60px",
-                    background: lightColor,
-                    fontFamily: "var(--font-family-cerapro-bold)",
-                    // textAlign: "center",
-                    textAlign: "center",
-                    justifyContent: "center",
-                    padding: "5px",
-                  }}
-                >
-                  <h3
+          {error ? (
+            <h1>{error}</h1>
+          ) : (
+            <form>
+              <Grid container spacing={2}>
+                {/* <> */}
+                <Grid item xs={5}>
+                  <div
                     style={{
+                      border: `4px solid ${darkColor}`,
+                      borderRadius: "39px",
+                      width: "60px",
+                      height: "60px",
+                      background: lightColor,
                       fontFamily: "var(--font-family-cerapro-bold)",
+                      // textAlign: "center",
                       textAlign: "center",
-                      color: darkColor,
+                      justifyContent: "center",
+                      padding: "5px",
                     }}
                   >
-                    {compteur}
-                  </h3>
-                </div>
-              </Grid>
-              <Grid xs={7}>
-                <h2
-                  style={{
-                    fontFamily: "var(--font-family-cerapro-bold)",
-                    color: "#1D1D1D",
-                  }}
-                >
-                  Question {page}{" "}
-                </h2>
-              </Grid>
-              {/* <Grid item xs={12}> */}
-              {quiz?.map((item) => {
-                return (
-                  <Grid
-                    item
-                    container
-                    textAlign="center"
-                    justifyContent="center"
-                    key={item._id}
+                    <h3
+                      style={{
+                        fontFamily: "var(--font-family-cerapro-bold)",
+                        textAlign: "center",
+                        color: darkColor,
+                      }}
+                    >
+                      {timer}
+                    </h3>
+                  </div>
+                </Grid>
+                <Grid xs={7}>
+                  <h2
+                    style={{
+                      fontFamily: "var(--font-family-cerapro-bold)",
+                      color: "#1D1D1D",
+                    }}
                   >
-                    <Grid xs={12} item>
-                      <h5>{item.tronc}</h5>
-                    </Grid>
-                    <Grid item>
-                      {item.propositions.map((proposition, index) => {
-                        return (
-                          <Grid
-                            xs={12}
-                            item
-                            key={index}
-                            style={{ display: "flex" }}
-                          >
-                            <Checkbox
-                              key={index}
-                              name="response"
-                              onChange={(e) => handleCheck(e, proposition._id)}
-                              value={answercandidate.response}
-                            />
-                            <h6
-                              value={answercandidate._id_proposition}
-                              name="_id_proposition"
+                    Question {page}{" "}
+                  </h2>
+                </Grid>
+                {/* <Grid item xs={12}> */}
+                {quiz?.map((item, index) => {
+                  return (
+                    <Grid
+                      item
+                      container
+                      textAlign="center"
+                      justifyContent="center"
+                    >
+                      <Grid xs={12} item>
+                        <h4
+                          style={{
+                            fontFamily: "var(--font-family-cerapro-bold)",
+                            color: "#1D1D1D",
+                          }}
+                        >
+                          {item.tronc}
+                        </h4>
+                      </Grid>
+                      <Grid item key={item._id}>
+                        {item.propositions.map((proposition, index) => {
+                          return (
+                            <Grid
+                              xs={12}
+                              item
+                              key={proposition._id}
+                              style={{ display: "flex" }}
                             >
-                              {proposition.content}
-                            </h6>
-                          </Grid>
-                        );
-                      })}
+                              <Checkbox
+                                key={proposition._id}
+                                name="response"
+                                onChange={
+                                  (e) =>
+                                    handleCheck(
+                                      e,
+                                      proposition._id,
+                                      proposition.question
+                                    )
+                                  // isChecked(answercandidate._id_proposition);
+                                }
+                                style={{
+                                  color: lightColor,
+                                }}
+                                value={answercandidate[index]}
+                                // checked={() => {
+                                //   answercandidate.map((answer) => {
+                                //     console.log(answer);
+                                //   });
+                                // }}
+                                // checked={}
+                                // value={answercandidate.response}
+                              />
+                              <h5
+                                value={answercandidate._id_proposition}
+                                name="_id_proposition"
+                                style={{
+                                  fontFamily:
+                                    "var(--font-family-cerapro-medium)",
+                                  color: darkColor,
+                                }}
+                              >
+                                {proposition.content}
+                              </h5>
+                            </Grid>
+                          );
+                        })}
+                      </Grid>
                     </Grid>
-                  </Grid>
-                );
-              })}
-            </Grid>
-            <Grid
-              item
-              style={{
-                marginLeft: "290px",
-                marginTop: "15px",
-                borderRadius: "33px",
-                // background: darkColor,
-                // opacity: "0.3",
-              }}
-            >
-              <Pagination
-                count={count}
-                variant="outlined"
-                showFirstButton
-                showLastButton
-                page={page}
-                onChange={handleChangePage}
+                  );
+                })}
+              </Grid>
+              <Grid
+                item
                 style={{
+                  marginLeft: "290px",
+                  marginTop: "15px",
                   borderRadius: "33px",
-                  color: lightColor,
-                  "& .Mui-selected": {
-                    backgroundColor: "transparent",
-                    color: "red",
-                  },
+                  // background: darkColor,
+                  // opacity: "0.3",
                 }}
-              />
-            </Grid>
-            {/* </Grid> */}
-          </form>
+              >
+                <Pagination
+                  count={count}
+                  variant="outlined"
+                  showFirstButton
+                  showLastButton
+                  page={page}
+                  onChange={handleChangePage}
+                  style={{
+                    borderRadius: "33px",
+                    color: lightColor,
+                    // "& .Mui-selected": {
+                    //   backgroundColor: "transparent",
+                    //   color: "red",
+                    // },
+                  }}
+                />
+              </Grid>
+            </form>
+          )}
           {/* </div> */}
         </ContentMenuItem>
       </Grid>
